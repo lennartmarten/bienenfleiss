cd8d98e7be3b691a1d914d8fdcc8b5d3186ea9c2
lennartmarten <89070109+lennartmarten@users.noreply.github.com>
2021-10-26 15:32:49 +0200

    Added new styling + JS

--- js/main.js
+++ js/main.js
@@ -1,38 +1,35 @@
-// Wenn der User scrollt, dann wird die Scroll fuction aktiviert
-window.onscroll = function() {
-    scrollFunction()
-};
-// Die Funktion wird angesprochen und ausgeführt.
-function scrollFunction() {
-    // Wenn 94px erreicht werden ODER oder 60px nach oben scrollt
-    if (document.body.scrollTop > 94 || document.documentElement.scrollTop > 60) {
-        // Dieses Sytling soll die Navigation übernehmen
-        document.getElementById("navbar").style.height = "54px";
-        // Wenn nicht dann soll der Wert von ?px übernommen werden
-    } else {
-        document.getElementById("navbar").style.height = "94px";
+$(document).ready(function() {
+    // Wenn der User scrollt, dann wird die Scroll fuction aktiviert
+    // Die Funktion wird angesprochen und ausgeführt.
+    function scrollFunction() {
+        mybutton = document.getElementById("myBtn");
+        // Wenn 94px erreicht werden ODER oder 60px nach oben scrollt
+        if (document.body.scrollTop > 94 || document.documentElement.scrollTop > 60) {
+            // Dieses Sytling soll die Navigation übernehmen
+            document.getElementById("navbar").style.height = "54px";
+            // Wenn nicht dann soll der Wert von ?px übernommen werden
+        } else {
+            document.getElementById("navbar").style.height = "94px";
+        }
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+
     }
-}
+
+    // When the user clicks on the button, scroll to the top of the document
+    $("#myBtn").on("click", function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 
+    });
 
+    window.onscroll = function() {
+        scrollFunction()
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
+    };
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
